f30f5ff6a10fc1bf88722149fa2e88e71ab31ab2
Alberto <albertjmrs@gmail.com>
2018-06-27 16:49:08 +0100

    Error handling added - Second Demo Ver

--- src/app/event-info/event-info.component.ts
+++ src/app/event-info/event-info.component.ts
@@ -16,5 +16,6 @@ export class EventInfoComponent implements OnInit {
 
   onClick() {
     this.sharedInfo.aux=!this.sharedInfo.aux;
+    this.getFromApi.error=false;
   }
 }
--- src/app/getfromapi.service.ts
+++ src/app/getfromapi.service.ts
@@ -8,16 +8,16 @@ import { Event } from './event';
 export class GetfromapiService {
 
   events: Event[] = new Array();
+  error: boolean = false;
 
   url = 'https://mock-api.drinks.test.siliconrhino.io/events/';
 
   constructor(private http: HttpClient) { }
 
   getEvent(id) {
-
     this.http.get<Event>(this.url+id).subscribe(
-      (data: Event) => this.events[0] = data
+      (data: Event) => this.events[0] = data,
+      error => this.error=true
     );
-
   }
 }
